e9f6f54a2391cb26163acc253cc467d6b9c65174
Shahrukh <ShahrukhMushtaq23@gmail.com>
2018-09-08 20:55:31 +0500

    update submit ad API

--- models/user-subscription.js
+++ models/user-subscription.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 const UserSubscription = mongoose.model('UserSubscription', new mongoose.Schema({
-    user_subscription: []
+    user_subscription: any
 }));
 module.exports = UserSubscription;
